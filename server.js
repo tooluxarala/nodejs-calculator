2496e12a9da1b3ad68b461cb75963f8a0c1f5508
DIARRA <boubs408@gmail.com>
2024-07-19 17:59:42 +0000

    Added nodejs-calculator in server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 const Calculator = require('./calculator');
 
 console.log("Addition de : 5 + 10 = " + Calculator.add(5, 10));
+console.log("Soustraction de : 7 - 3 = " + Calculator.subtract(7, 3));
