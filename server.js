1ac86a166d2c72673ce8727ea16555bff4d8c1e9
Fallou Fall <ffall@gravitasgroup.io>
2024-07-20 13:52:49 +0000

    - Added other calculator operations (sub, mul, div) - Updated README

--- server.js
+++ server.js
@@ -15,7 +15,26 @@ console.log("Div: 9 / 3 = " + Calculator.divide(9, 3))
 
 //console.log("Div: 9 / 0 = " + Calculator.divide(9, 0))
 
-
+const processOperation = (pathname, params) => {
+    let a = parseInt(params.a);
+    let b = parseInt(params.b);
+    let operationResult = {
+        "operation": pathname.substring(1),
+        params
+    };
+    if (pathname === '/add') {
+        operationResult.result = Calculator.add(a, b);
+    } else if (pathname === '/subtract') {
+        operationResult.result = Calculator.subtract(a, b);
+    } else if (pathname === '/multiply') {
+        operationResult.result = Calculator.multiply(a, b);
+    } else if (pathname === '/divide') {
+        operationResult.result = Calculator.divide(a, b);
+    } else {
+        throw new Error("Innvalid operation '" + operationResult.operation + "'");
+    }
+    return operationResult;
+}
 
 
 const hostname = '127.0.0.1';
@@ -27,7 +46,7 @@ const server = createServer((req, res) => {
     // Note: This is for debuging purpose
     //console.log('parsedUrl: ' + JSON.stringify(parsedUrl))
 
-    if (req.method === 'POST' && parsedUrl.pathname === '/add') {
+    if (req.method === 'POST') {
         let body = [];
         req.on('error', err => {
             console.error(err);
@@ -40,18 +59,20 @@ const server = createServer((req, res) => {
             // At this point, we have the headers, method, url and body, and can now
             // do whatever we need to in order to respond to this request.
 
-            let params = JSON.parse(body)
-            let a = parseInt(params.a);
-            let b = parseInt(params.b);
+            try{
 
-            let result = {
-                "operation": "Add",
-                "params" : params,
-                "result": Calculator.add(a, b)
+                let params = JSON.parse(body)
+
+                let result = processOperation(parsedUrl.pathname, params)
+    
+                res.statusCode = 200
+    
+                res.end(JSON.stringify(result))
+            } catch(error){
+                console.log("Failed processing request: " + error );
+                res.statusCode = 400;
+                res.end("Failed processing request: " + error);
             }
-            res.statusCode = 200
-
-            res.end(JSON.stringify(result))
         });
 
     } else {
